34c50c96a3164c7316510d76355320851a691515
radioegor146 <radioegor146@yandex.ru>
2019-03-10 13:10:49 +0300

    equality fix

--- DNMPLibrary/Interaction/Protocol/EndPointImpl/RealIPEndPoint.cs
+++ DNMPLibrary/Interaction/Protocol/EndPointImpl/RealIPEndPoint.cs
@@ -1,15 +1,32 @@
-﻿using System.Net;
+﻿using System.Collections.Generic;
+using System.Net;
 
 namespace DNMPLibrary.Interaction.Protocol.EndPointImpl
 {
 
     public class RealIPEndPoint : IEndPoint
     {
-        public IPEndPoint RealEndPoint;
+        public readonly IPEndPoint RealEndPoint;
 
         public RealIPEndPoint(IPEndPoint realEndPoint)
         {
             RealEndPoint = realEndPoint;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is RealIPEndPoint point &&
+                   EqualityComparer<IPEndPoint>.Default.Equals(RealEndPoint, point.RealEndPoint);
+        }
+
+        public override int GetHashCode()
+        {
+            return -1407700588 + EqualityComparer<IPEndPoint>.Default.GetHashCode(RealEndPoint);
+        }
+
+        public override string ToString()
+        {
+            return RealEndPoint.ToString();
         }
     }
 }
